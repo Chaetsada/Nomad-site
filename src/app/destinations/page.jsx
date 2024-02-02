b7127215767478581f589bf318934a5291a8a5c3
JKnochu <chaetsada.k@outlook.com>
2024-02-03 02:15:35 +0700

    bug: fix bug destination page

--- src/app/destinations/page.jsx
+++ src/app/destinations/page.jsx
@@ -4,68 +4,8 @@ import React from "react";
 import { CiCalendar } from "react-icons/ci";
 import Newsletter from "../Components/Newsletter";
 
-const oneWeekTrip = [
-  {
-    city: "Seoul,South Korea",
-    date: "6/1/23 - 6/7/23",
-    price: "$1300",
-    path: "seoul-south-korea",
-    img: "https://images.unsplash.com/photo-1548115184-bc6544d06a58?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    city: "Tokyo,Japan",
-    date: "6/7/23 - 6/14/23",
-    price: "$1200",
-    path: "tokyo-japan",
-    img: "https://images.unsplash.com/photo-1542640244-7e672d6cef4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    city: "Bora Bora,French Polynesia",
-    date: "6/14/23 - 6/21/23",
-    price: "$2400",
-    path: "bora-bora-french-polynesia",
-    img: "https://images.unsplash.com/photo-1589197331516-4d84b72ebde3?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
-const twoWeekTrip = [
-  {
-    city: "Positano,Italy",
-    date: "6/1/23 - 6/14/23",
-    price: "$3200",
-    path: "positano-italy",
-    img: "https://images.unsplash.com/photo-1594881497142-08fdfdfc4074?q=80&w=1315&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    city: "Satorini,Greece",
-    date: "6/14/23 - 6/28/23",
-    price: "$3200",
-    path: "santorini-greece",
-    img: "https://images.unsplash.com/photo-1570077188670-e3a8d69ac5ff?q=80&w=2038&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
-const oneMonthTrip = [
-  {
-    city: "Austin,Texas",
-    date: "6/1/23 - 7/1/23",
-    price: "$4800",
-    path: "austin-texas",
-    img: "https://images.unsplash.com/photo-1530089711124-9ca31fb9e863?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    city: "Bali,Indonisia",
-    date: "7/1/23 - 8/1/23",
-    price: "$4000",
-    path: "bali-indonesias",
-    img: "https://images.unsplash.com/photo-1559628233-100c798642d4?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    city: "San Francisco,California",
-    date: "8/1/23 - 9/1/23",
-    price: "$5200",
-    path: "san-francisco-california",
-    img: "https://images.unsplash.com/photo-1605403919557-efd20173336d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
+import { oneWeekTrip,twoWeekTrip,oneMonthTrip } from "../libs/data";
+
 
 const Destinations = () => {
   return (
